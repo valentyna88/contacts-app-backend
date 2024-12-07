b58e403dfafa6750a5b0858ae7fdccbea59c7089
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-12-07 02:25:23 +0200

    Add controllers

---
+++ src/controllers/contacts.js
@@ -0,0 +1,29 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getContactsController = async (req, res) => {
+  const contacts = await getAllContacts();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
+};
+
+export const getContactByIdController = async (req, res) => {
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+
+  if (!contact) {
+    res.status(404).json({
+      message: 'Contact not found',
+    });
+    return;
+  }
+
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  });
+};
--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
+import contactsRouter from './routers/contacts.js';
+
 import { env } from './utils/env.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
 
 export const setupServer = () => {
   const app = express();
@@ -20,33 +21,13 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Welcome!',
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
+  app.use(contactsRouter);
 
   app.use((req, res, next) => {
     res.status(404).json({
