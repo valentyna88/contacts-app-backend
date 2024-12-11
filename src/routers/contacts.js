eb5c1cbc967e2b74dd1d8e48f186b7accc499897
Valentyna_Taranchuk <valyataranchuk8@gmail.com>
2024-12-11 17:11:26 +0200

    Add POST request route

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import {
   getContactByIdController,
   getContactsController,
+  createContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -10,5 +11,7 @@ const router = Router();
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
 
 export default router;
